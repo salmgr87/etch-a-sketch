dd0da7c9982ebef9893e3949aa68fc2775633588
salmgr87 <96929835+salmgr87@users.noreply.github.com>
2022-11-04 21:45:18 -0400

    Change size of blocks based on number of rows

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
 const container = document.querySelector(".container");
 console.log(container);
 
+let rows = prompt();
+let totalGridItems = rows*rows;
+let fontSizez = 400/rows;
+let fontSizeString = fontSizez + "px";
 
-for (i=0; i<100; i++) {
+//changes the container to have the correct rows
+let columns = 'repeat: (' + rows + ", 1fr)";
+let correctRows = document.getElementsByClassName('container');
+correctRows[0].style.gridTemplateColumns = columns;
+
+
+//creates grid of blocks
+for (i=0; i<(totalGridItems); i++) {
         const item = document.createElement('div');
         item.classList.add('gridItem');
         container.appendChild(item);
+        
 }
+
+//changes the class of each block to reflect the correct size in px
+let items = document.getElementsByClassName('gridItem');
+for (i=0; i<(totalGridItems); i++) {
+        const item = items[i];
+        item.style.fontSize = fontSizeString;
+}
+
+
+
 
 document.addEventListener('click', function handleClick(event) {
         event.target.classList.toggle('clicked');
